8fe194a48624311e763085f6f9133422ac67f7c7
Rocetti Rinke <ro7rinke@gmail.com>
2020-07-28 13:58:38 -0300

    contagem de arquivo e diretorios

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const cp = require('child_process')
 
+let dirNumbers = 0
+let fileNumbers = 0
+
 const copyFile = async (filePath, destPath) => {
     const pathArray = filePath.split('/')
     const fileName = pathArray.pop()
@@ -18,14 +21,18 @@ const readDir = (dirPath) => {
     fs.readdir(dirPath, 'utf-8', (error, files) => {
         if(error) console.log(error)
         else{
-            console.log(files)
             for(let index in files){
                 fs.stat(`${pathAux}/${files[index]}`, 'utf-8', (err, stats) => {
                     if(err) console.log(err)
                     else{
                         if(stats.isDirectory()) {
                             readDir(`${pathAux}/${files[index]}`)
-                        }   
+                            dirNumbers++
+                            console.log('dir: '+dirNumbers)
+                        }else{
+                            fileNumbers++
+                            console.log('file: '+fileNumbers)
+                        }
                     }
                 })
             }
@@ -34,7 +41,7 @@ const readDir = (dirPath) => {
 }
 
 const main = () => {
-    readDir('./test')
+    readDir('D:/Documents')
 }
 
 main()
